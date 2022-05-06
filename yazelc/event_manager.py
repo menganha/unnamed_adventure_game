1ba4f85c78c819fd6e3e57d64bb19a4898215aab
menganha <34135296+menganha@users.noreply.github.com>
2022-05-06 22:19:44 +0200

    Changes the event manager to a class

--- yazelc/event_manager.py
+++ yazelc/event_manager.py
@@ -1,27 +1,40 @@
-""" Copied from
-https://jakubszwajka.github.io/How-to-build-event-system-python/_
-"""
-from collections import defaultdict as _defaultdict
-from typing import Callable as _Callable
+from collections import defaultdict
+from typing import Callable
 
-from yazelc.event_type import EventType as _EventType
-
-# Uses defaultdict to initialize a list when using a new (missing) key in the dict
-subscribers = _defaultdict(list)
+from yazelc.event_type import EventType
 
 
-def subscribe(event_type: _EventType, listener: _Callable):
-    global subscribers
-    subscribers[event_type].append(listener)
+class EventManager:
+    """ Copied from
+    https://jakubszwajka.github.io/How-to-build-event-system-python/_
+    """
+
+    def __init__(self):
+        # Uses defaultdict to initialize a list when using a new (missing) key in the dict
+        self.subscribers = defaultdict(list)
+
+    def subscribe(self, event_type: EventType, listener: Callable):
+        self.subscribers[event_type].append(listener)
+
+    def post_event(self, event_type: EventType, *args, **kwargs):
+        if event_type in self.subscribers:
+            for listener in self.subscribers[event_type]:
+                listener(*args, **kwargs)
+
+    def clear(self):
+        self.subscribers = defaultdict(list)
 
 
-def post_event(event_type: _EventType, *args, **kwargs):
-    global subscribers
-    if event_type in subscribers:
-        for listener in subscribers[event_type]:
-            listener(*args, **kwargs)
+_event_manager = EventManager()
+
+
+def subscribe(event_type: EventType, listener: Callable):
+    _event_manager.subscribe(event_type, listener)
+
+
+def post_event(event_type: EventType, *args, **kwargs):
+    _event_manager.post_event(event_type, *args, **kwargs)
 
 
 def clear_subscribers():
-    global subscribers
-    subscribers = _defaultdict(list)
+    _event_manager.clear()
